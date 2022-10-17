086e8c5f1f14bf443e4793d0ae04b1b83c1b8626
Kefiloe Moshoaliba <90796051+Moshoaliba@users.noreply.github.com>
2022-10-18 00:05:58 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Charges_in_a_Luxury_Resort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDays = new System.Windows.Forms.Label();
+            this.lblResCharges = new System.Windows.Forms.Label();
+            this.lblSpaAndHealth = new System.Windows.Forms.Label();
+            this.lblCarCharge = new System.Windows.Forms.Label();
+            this.lblBill = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtInput2 = new System.Windows.Forms.TextBox();
+            this.txtInput4 = new System.Windows.Forms.TextBox();
+            this.txtInput5 = new System.Windows.Forms.TextBox();
+            this.txtInput3 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDays
+            // 
+            this.lblDays.AutoSize = true;
+            this.lblDays.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDays.Location = new System.Drawing.Point(22, 21);
+            this.lblDays.Name = "lblDays";
+            this.lblDays.Size = new System.Drawing.Size(107, 15);
+            this.lblDays.TabIndex = 0;
+            this.lblDays.Text = "No. of days Stayed";
+            // 
+            // lblResCharges
+            // 
+            this.lblResCharges.AutoSize = true;
+            this.lblResCharges.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResCharges.Location = new System.Drawing.Point(22, 74);
+            this.lblResCharges.Name = "lblResCharges";
+            this.lblResCharges.Size = new System.Drawing.Size(191, 15);
+            this.lblResCharges.TabIndex = 1;
+            this.lblResCharges.Text = "Restaurant charges including VAT";
+            // 
+            // lblSpaAndHealth
+            // 
+            this.lblSpaAndHealth.AutoSize = true;
+            this.lblSpaAndHealth.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSpaAndHealth.Location = new System.Drawing.Point(22, 133);
+            this.lblSpaAndHealth.Name = "lblSpaAndHealth";
+            this.lblSpaAndHealth.Size = new System.Drawing.Size(192, 15);
+            this.lblSpaAndHealth.TabIndex = 2;
+            this.lblSpaAndHealth.Text = "Spa and Health Treatment charge";
+            // 
+            // lblCarCharge
+            // 
+            this.lblCarCharge.AutoSize = true;
+            this.lblCarCharge.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCarCharge.Location = new System.Drawing.Point(22, 199);
+            this.lblCarCharge.Name = "lblCarCharge";
+            this.lblCarCharge.Size = new System.Drawing.Size(131, 15);
+            this.lblCarCharge.TabIndex = 3;
+            this.lblCarCharge.Text = "Charges for Car Rental";
+            // 
+            // lblBill
+            // 
+            this.lblBill.AutoSize = true;
+            this.lblBill.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBill.Location = new System.Drawing.Point(22, 258);
+            this.lblBill.Name = "lblBill";
+            this.lblBill.Size = new System.Drawing.Size(191, 15);
+            this.lblBill.TabIndex = 4;
+            this.lblBill.Text = "Medication and Rehabilitation Bill";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(302, 21);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(134, 21);
+            this.txtInput.TabIndex = 5;
+            // 
+            // txtInput2
+            // 
+            this.txtInput2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput2.Location = new System.Drawing.Point(302, 74);
+            this.txtInput2.Name = "txtInput2";
+            this.txtInput2.Size = new System.Drawing.Size(134, 21);
+            this.txtInput2.TabIndex = 6;
+            // 
+            // txtInput4
+            // 
+            this.txtInput4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput4.Location = new System.Drawing.Point(302, 193);
+            this.txtInput4.Name = "txtInput4";
+            this.txtInput4.Size = new System.Drawing.Size(134, 21);
+            this.txtInput4.TabIndex = 7;
+            // 
+            // txtInput5
+            // 
+            this.txtInput5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput5.Location = new System.Drawing.Point(302, 252);
+            this.txtInput5.Name = "txtInput5";
+            this.txtInput5.Size = new System.Drawing.Size(134, 21);
+            this.txtInput5.TabIndex = 8;
+            // 
+            // txtInput3
+            // 
+            this.txtInput3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput3.Location = new System.Drawing.Point(302, 127);
+            this.txtInput3.Name = "txtInput3";
+            this.txtInput3.Size = new System.Drawing.Size(134, 21);
+            this.txtInput3.TabIndex = 9;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(25, 364);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(411, 139);
+            this.listBox1.TabIndex = 10;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(25, 306);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(158, 29);
+            this.btnCalculate.TabIndex = 11;
+            this.btnCalculate.Text = "Calculate Total Charges";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(319, 306);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(90, 29);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 520);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtInput3);
+            this.Controls.Add(this.txtInput5);
+            this.Controls.Add(this.txtInput4);
+            this.Controls.Add(this.txtInput2);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblBill);
+            this.Controls.Add(this.lblCarCharge);
+            this.Controls.Add(this.lblSpaAndHealth);
+            this.Controls.Add(this.lblResCharges);
+            this.Controls.Add(this.lblDays);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDays;
+        private System.Windows.Forms.Label lblResCharges;
+        private System.Windows.Forms.Label lblSpaAndHealth;
+        private System.Windows.Forms.Label lblCarCharge;
+        private System.Windows.Forms.Label lblBill;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtInput2;
+        private System.Windows.Forms.TextBox txtInput4;
+        private System.Windows.Forms.TextBox txtInput5;
+        private System.Windows.Forms.TextBox txtInput3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿//Moshoaliba,K-34316345
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Charges_in_a_Luxury_Resort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            decimal days = decimal.Parse(txtInput.Text);
+            decimal charge_1 = decimal.Parse(txtInput2.Text);
+            decimal charge_2 = decimal.Parse(txtInput3.Text);
+            decimal charge_3 = decimal.Parse(txtInput4.Text);
+            decimal charge_4 = decimal.Parse(txtInput5.Text);
+
+
+            CalcStayCharges(days);
+
+            CalcMiscCharges(charge_1, charge_2, charge_3, charge_4);
+
+            CalcTotalCharges(days, charge_1, charge_2, charge_3, charge_4);
+        }
+
+        private decimal CalcStayCharges(decimal days)
+        {
+            const decimal Charge_for_stay = 550.00m;
+            decimal charge = days * Charge_for_stay;
+
+            listBox1.Items.Add("Stay charges : " + charge.ToString("C"));
+
+            return charge;
+
+        }
+
+        private decimal CalcMiscCharges(decimal charge_1, decimal charge_2 ,decimal charge_3, decimal charge_4)
+        {
+            decimal total = charge_1 + charge_3 + charge_2 + charge_4;
+
+            listBox1.Items.Add("Miscellaneuos charges : " + total.ToString("C"));
+
+            return total;
+        }
+
+        private decimal CalcTotalCharges(decimal days ,decimal charge_1, decimal charge_2, decimal charge_3, decimal charge_4)
+        {
+            const decimal Charge_for_stay = 550.00m;
+            decimal charge = days * Charge_for_stay;
+
+            decimal all_charges = charge_1 + charge_3 + charge_2 + charge_4 + charge;
+
+            listBox1.Items.Add("Total Bill : " + all_charges.ToString("C"));
+
+            return all_charges;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
